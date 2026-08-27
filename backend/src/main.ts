f6f83809234ab476f64df354f3db074347eef3f7
EloiEdu <eloieduardo.rs@gmail.com>
2026-08-27 19:03:42 -0300

    feat: frontend cleaner code refactor

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
